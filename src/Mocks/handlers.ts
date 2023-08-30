660dc180f2fd58823656f54247ee1b6c9c6487f7
Akash Agrawal <akashagrawal5012@gmail.com>
2023-08-30 09:58:38 +0530

    #mock data setup.

---
+++ src/Mocks/handlers.ts
@@ -0,0 +1,18 @@
+import { rest } from "msw";
+import { productMockData } from "../Data/productMock";
+import {productDataType, productFilterData} from '../Data/productType';
+
+export const handlers = [
+  rest.get("/products", (req, res, ctx) => {
+    console.log(req);
+    return res(ctx.status(200), ctx.json(productMockData));
+  }),
+  rest.get("/products/datatype", (req, res, ctx) => {
+    console.log(req);
+    return res(ctx.status(200), ctx.json(productDataType));
+  }),
+  rest.get("/products/filtertype", (req, res, ctx) => {
+    console.log(req);
+    return res(ctx.status(200), ctx.json(productFilterData));
+  }),
+];
