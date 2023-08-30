b843ecde90844047ff3e427066b86f16e9e6c821
Akash Agrawal <akashagrawal5012@gmail.com>
2023-08-30 15:42:45 +0530

    #Added small react plugin.

--- src/Pages/ForgotPassword.tsx
+++ src/Pages/ForgotPassword.tsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import "../Styles/ForgotPassword.scss";
 import { Box, Button, Grid, Paper, TextField, Typography } from "@mui/material";
 import LoadingButton from "@mui/lab/LoadingButton";
+import React from "react";
 
 // snackbar
 import { useSnackbar } from "../Contexts/SnackbarProvider";
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { worker } from "./Mocks/browser";
 import "./index.css";
+import React from "react";
 
 // Used only during development
 if (process.env.NODE_ENV === "development") {
