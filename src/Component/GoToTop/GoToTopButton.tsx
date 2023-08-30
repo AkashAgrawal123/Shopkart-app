5f4558738c84b84378253ea6231899cf09f6446a
Akash Agrawal <akashagrawal5012@gmail.com>
2023-08-30 11:47:46 +0530

    #changes

--- src/Component/GoToTop/GoToTopButton.tsx
+++ src/Component/GoToTop/GoToTopButton.tsx
@@ -1,41 +1,43 @@
-import { useEffect, useState } from 'react';
-import './GoToTopButton.scss';
+import { useEffect, useState } from "react";
+import "./GoToTopButton.scss";
 import { FaArrowUp } from "react-icons/fa";
-import '../../styles/globalStyle.scss';
+import "../../styles/globalStyle.scss";
+import React from "react";
 
 const GoToTopButton = () => {
   const [isVisible, setIsVisible] = useState(false);
-  
+
   const goToTopBtn = () => {
-   window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-  }
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
   const listenToScroll = () => {
     const heightToHide = 300;
-    const windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    
-    if(windowScroll > heightToHide){
-        setIsVisible(true);
+    const windowScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    if (windowScroll > heightToHide) {
+      setIsVisible(true);
     } else {
-        setIsVisible(false);
+      setIsVisible(false);
     }
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', listenToScroll);
-    return () => window.removeEventListener('scroll', listenToScroll);
+    window.addEventListener("scroll", listenToScroll);
+    return () => window.removeEventListener("scroll", listenToScroll);
   }, []);
 
-  return(
+  return (
     <>
-      <section className='goToTop__button'>
+      <section className="goToTop__button">
         {isVisible && (
-          <div className='goToTop__button--arrow' onClick={goToTopBtn}>
-            <FaArrowUp className='goToTop__button--arrow-icon' />
+          <div className="goToTop__button--arrow" onClick={goToTopBtn}>
+            <FaArrowUp className="goToTop__button--arrow-icon" />
           </div>
         )}
       </section>
     </>
-  )
+  );
 };
 
 export default GoToTopButton;
