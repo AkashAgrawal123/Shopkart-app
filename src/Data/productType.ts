eba9ce765f516b02ff7b575f688b8a9ee2e8caf9
Akash Agrawal <akashagrawal5012@gmail.com>
2023-08-30 09:57:58 +0530

    #Addec mock data into the project.

---
+++ src/Data/productType.ts
@@ -0,0 +1,104 @@
+export const productDataType = [
+  {
+    id: 1,
+    image: "./productType/furniture.png",
+    name: "Furniture",
+    noOfItem: 240,
+    altText: "Sofa's",
+    category: "Furniture",
+    isActive: false,
+  },
+  {
+    id: 2,
+    image: "./fashion/fashion3.jpg",
+    name: "Fashion",
+    noOfItem: 190,
+    altText: "Hand Bag",
+    category: "Fashion",
+    isActive: false,
+  },
+  {
+    id: 3,
+    image: "./productType/shoes.png",
+    name: "Sneakers",
+    noOfItem: 321,
+    altText: "Shoe",
+    category: "Sneakers",
+    isActive: false,
+  },
+  {
+    id: 4,
+    image: "./productType/headphone.png",
+    name: "Gadgets",
+    noOfItem: 260,
+    altText: "Headphone",
+    category: "gadgets",
+    isActive: false,
+  },
+  {
+    id: 5,
+    image: "./fitness/fitness2.jpg",
+    name: "Fitness",
+    noOfItem: 280,
+    altText: "Laptop",
+    category: "Toys",
+    isActive: false,
+  },
+  {
+    id: 6,
+    image: "./productType/book.png",
+    name: "Education",
+    noOfItem: 570,
+    altText: "Book",
+    category: "Education",
+    isActive: false,
+  },
+];
+
+export const productFilterData = [
+  {
+    id: 1,
+    name: "All Items",
+    isActive: false,
+  },
+  {
+    id: 2,
+    name: "Gadgets",
+    isActive: false,
+  },
+  {
+    id: 3,
+    name: "Toys",
+    isActive: false,
+  },
+  {
+    id: 4,
+    name: "Education",
+    isActive: false,
+  },
+  {
+    id: 5,
+    name: "Beauty",
+    isActive: false,
+  },
+  {
+    id: 6,
+    name: "Fitness",
+    isActive: false,
+  },
+  {
+    id: 7,
+    name: "Furniture",
+    isActive: false,
+  },
+  {
+    id: 8,
+    name: "Sneakers",
+    isActive: false,
+  },
+  {
+    id: 9,
+    name: "Fashion",
+    isActive: false,
+  },
+];
