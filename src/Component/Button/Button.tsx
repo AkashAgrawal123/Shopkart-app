dd63111c4dfb1232790b78c4af9fb2492ebe391d
Akash Agrawal <akashagrawal5012@gmail.com>
2023-08-30 09:50:40 +0530

    #Added component list into the setup

---
+++ src/Component/Button/Button.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  text-decoration: none;
+  max-width: auto;
+  // border: 1px solid #000000;
+  text-align: center;
+  cursor: pointer;
+`;
---
+++ src/Component/Skeleton-loading/SkeletonLoading.tsx
@@ -0,0 +1,38 @@
+import "./SkeletonLoading.scss";
+import "../../styles/globalStyle.scss";
+
+interface SkeletonLoadingPropsInterface {
+  count: number;
+}
+
+const SkeletonLoading = (props: SkeletonLoadingPropsInterface) => {
+  const { count } = props;
+
+  const skeletonElement = Array.from({ length: count }, (_, index) => (
+    <div key={index} className="shopCart-skeleton__loading--content ">
+      <div className="shopCart-skeleton__loading--card"></div>
+      <div className="shopCart-skeleton__loading--heading-wrapper">
+        <div className="shopCart-skeleton__loading--name"></div>
+        <div className="shopCart-skeleton__loading--price"></div>
+      </div>
+      <div className="shopCart-skeleton__loading--description"></div>
+      <div className="shopCart-skeleton__loading--rating">
+        <div className="shopCart-skeleton__loading--star-rating"></div>
+        <div className="shopCart-skeleton__loading--reviews"></div>
+      </div>
+      <div className="shopCart-skeleton__loading--button"></div>
+    </div>
+  ));
+
+  return (
+    <>
+      <div className="shopCart-skeleton__loading shopCart-skeleton__loading--animation">
+        <div className="shopCart-skeleton__loading--wrapper">
+          {skeletonElement}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SkeletonLoading;
